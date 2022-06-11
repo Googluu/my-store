2f8465ac4e87b583fb3cf98080beae9bd64c77be
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-06-10 21:06:43 -0500

    Sequelize libs db config

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,11 +1,9 @@
 function logErrors(err, req, res, next) {
-  console.log('logErrors');
   console.error(err);
   next(err);
 }
 
 function errorHandler(err, req, res, next) {
-  console.log('errorHandler');
   res.status(500).json({
     message: err.message,
     stack: err.stack
@@ -19,5 +17,6 @@ function boomErrorHandler(err, req, res, next) {
   }
   next(err);
 }
+
 
 module.exports = { logErrors, errorHandler, boomErrorHandler };
